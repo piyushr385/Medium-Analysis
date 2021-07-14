dc8a831cf2ecedb6488210230f99748ec3f2698f
piyushr385 <59258668+piyushr385@users.noreply.github.com>
2021-07-14 19:47:30 +0530

    Initial commit

---
+++ Notebooks/scrape_master.py
@@ -0,0 +1,25 @@
+import os
+from medium_scraper import *
+
+#TO RUN THIS FILE YOU NEED SELENIUM, BEAUTIFULSOUP, PANDAS, DATETIME, REGEX, AND OS
+#JUST GO TO THE COMMAND LINE AND
+#SET WORKING DIRECTORY TO THE DIRECTORY WITH BOTH MEDIUM_SCRAPER.PY AND SCRAPE_MASTER.PY
+#THEN ENTER COMMAND "$python scrape_master.py"
+
+
+#ADD THE TAGS TO SCRAPE HERE
+tags = ["data-science","python","ai","machine-learning","deep-learning","big-data","computer-vision","nlp"]
+
+
+#ADD THE DATES TO SCRAPE HERE
+yearstart=2019
+monthstart=1
+yearstop=2019
+monthstop=12
+
+#LOOPS THROUGH ALL LISTED-TAGS AND SCRAPES DATA OFF OF MEDIUM/TAG/archive
+#SAVES THE FILES TO /TAG_SCRAPES/ IN CSV FORMAT
+for tag in tags:
+    scrape_tag(tag, yearstart, monthstart, yearstop, monthstop)
+    print("Done with tag: ", tag)
+print("done")
